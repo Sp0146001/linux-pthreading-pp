c202ca12974dc89acb649677d84ddeb7cc0e8089
sp0146001 <sp0146001@gmail.com>
2026-09-19 17:13:31 +0300

    sample functions как в презе Лек-ПП-1 про потоки POSIX'a

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ bool isInside(double x, double y, double r)
 size_t calc(double r, size_t tests, size_t seed)
 {
   std::mt19937 gen(seed);
-  std::uniform_real_distribution<double> dist(-r, r);
+  std::uniform_real_distribution< double > dist(-r, r);
   size_t hits = 0;
   for (size_t i = 0; i < tests; ++i)
   {
@@ -22,4 +22,11 @@ size_t calc(double r, size_t tests, size_t seed)
     }
   }
   return hits;
+}
+
+void* sample(void* data)
+{
+  auto p = static_cast< double* >(data);
+  size_t hits = calc(p[0], static_cast<size_t>(p[1]), static_cast<size_t>(p[2]));
+  return reinterpret_cast< void* >(hits);
 }
