66272eb5f5368d64c550185657bad0581dbaf304
sp0146001 <sp0146001@gmail.com>
2026-09-19 17:08:54 +0300

    calculation of area method's Monte Carlo(for one thread)

--- main.cpp
+++ main.cpp
@@ -4,8 +4,22 @@
 #include <cmath>
 #include <cstring>
 
-
 bool isInside(double x, double y, double r)
 {
   return x * x + y * y <= r * r;
+}
+
+size_t calc(double r, size_t tests, size_t seed)
+{
+  std::mt19937 gen(seed);
+  std::uniform_real_distribution<double> dist(-r, r);
+  size_t hits = 0;
+  for (size_t i = 0; i < tests; ++i)
+  {
+    if (isInside(dist(gen), dist(gen), r))
+    {
+      ++hits;
+    }
+  }
+  return hits;
 }
